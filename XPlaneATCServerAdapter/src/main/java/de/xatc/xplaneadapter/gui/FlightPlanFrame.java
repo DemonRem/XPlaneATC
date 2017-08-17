b0b2ad5fa314a201fddc27b8b24c89c2adc1a6c2
Mirko Bubel <mirko_bubel@hotmail.com>
2017-08-17 18:26:38 +0200

    changed debugging messages to see linewrap

--- XPlaneATCServerAdapter/src/main/java/de/xatc/xplaneadapter/gui/FlightPlanFrame.java
+++ XPlaneATCServerAdapter/src/main/java/de/xatc/xplaneadapter/gui/FlightPlanFrame.java
@@ -346,7 +346,7 @@ public class FlightPlanFrame extends javax.swing.JFrame {
 
     //TRACKDATAINNB_START
     //itemName="pilotSendNewFlightPlan"
-    //comment="initially send new FlightPlan mit extra langem schubidu um zu sehen, ob hier eine zeile bricht oder scrollbar ist"
+    //comment="initially send new FlightPlan from XPlane Adapter"
     //step=1
     //itemType="Method"
     //methodName="sendButtonActionPerformed"
